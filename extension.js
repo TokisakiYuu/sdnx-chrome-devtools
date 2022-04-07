d715f91d31bc57d93b2a813d2e7d60ac58e08c86
zhaoliping <tokisakiyuu@qq.com>
2022-04-08 01:20:00 +0800

    update

--- extension.js
+++ extension.js
@@ -33,4 +33,18 @@ function collectComplete(rows) {
 function createButtonForRow(row) {
     const phone = row.querySelector('td:nth-child(3)').innerText
     console.log(`create button => phone: ${phone}`);
+    const operations = row.querySelector('td:nth-child(5)')
+    const separator = dom('<div class="ant-divider ant-divider-vertical" role="separator"></div>')
+    const button = dom(`<a>登录此账号</a>`)
+    operations.appendChild(separator)
+    operations.appendChild(button)
+    button.addEventListener('click', () => {
+        window.open('https://www.baidu.com/s?wd=hello')
+    })
+}
+
+function dom(str) {
+    const div = document.createElement('div')
+    div.innerHTML = str
+    return div.firstElementChild
 }
