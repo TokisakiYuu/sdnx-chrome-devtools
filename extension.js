9bafa6f449283f3f3522629214a8072a2f8c6faf
zhaoliping <tokisakiyuu@qq.com>
2022-04-09 00:11:29 +0800

    解决数据抓取和页面路由之间的冲突

--- extension.js
+++ extension.js
@@ -1,81 +1,63 @@
-// 租户列表页面
-if (/\/auth\/services\/servicesList\/\d+\/account/.test(location.pathname)) {
-
-    const rows = []
-    let timer = null
-
-    // 抓取表格数据
-    document.addEventListener('DOMNodeInserted', event => {
-        const target = event.target
-        if (!(target instanceof HTMLElement)) return
-        if (target.classList.contains('ant-table-row')) {
-            rows.push(target)
-            if (timer) {
-                clearTimeout(timer)
-            }
-            timer = setTimeout(() => {
-                collectComplete([...rows])
-                rows.length = 0
-            }, 200)
-        }
+/**
+ * 创建按钮
+ * @param {HTMLTableRowElement} row 
+ */
+function createButtonForRow(row, data) {
+    const operations = row.querySelector('td:nth-child(5)')
+    const separator = dom('<div class="ant-divider ant-divider-vertical" role="separator"></div>')
+    const button = dom(`<a>登录此账号</a>`)
+    operations.appendChild(separator)
+    operations.appendChild(button)
+    button.addEventListener('click', () => {
+        // TODO 发起登录请求，得到token，写入localStorage，跳转页面
+        console.log(data)
     })
+    document.addEventListener('popstate', () => {
+        separator.remove()
+        button.remove()
+    })
+}
 
-    document.addEventListener('DOMCharacterDataModified', event => {})
-
-    function collectComplete(rows) {
-        // 每行创建按钮
-        rows.forEach(createButtonForRow)
-        // fetch api monkey patch抓取获取loginKey的请求
-        fetchTrap()
+function fetchTrap(callback) {
+    const oldFetch = window.fetch
+    const newFetch = async function(...args) {
+        const [url] = args
+        const res = await oldFetch(...args)
+        const myRes = res.clone()
+        const json = await myRes.json()
+        if (json.code === 10000) {
+            callback(url, json.info)
+        }
+        return res
     }
+    Reflect.set(window, 'fetch', newFetch)
+}
 
-    let requestAutoLogin = false
-    
-    /**
-     * 创建按钮
-     * @param {HTMLTableRowElement} row 
-     */
-    function createButtonForRow(row) {
-        const phone = row.querySelector('td:nth-child(3)').innerText
-        const operations = row.querySelector('td:nth-child(5)')
-        const separator = dom('<div class="ant-divider ant-divider-vertical" role="separator"></div>')
-        const button = dom(`<a>登录此账号</a>`)
-        operations.appendChild(separator)
-        operations.appendChild(button)
-        button.addEventListener('click', () => {
-            requestAutoLogin = true
-            sessionStorage.setItem('sdnx_chrome_devtools_query_phone', phone)
-            operations.querySelector('a:nth-of-type(4)').click()
+function dom(str) {
+    const div = document.createElement('div')
+    div.innerHTML = str
+    return div.firstElementChild
+}
+
+const sleep = (time) => new Promise(resolve => setTimeout(resolve, time))
+
+// 是否是租户列表页面
+function isTenantAccountPage() {
+    return /\/auth\/services\/servicesList\/\d+\/account/.test(location.pathname)
+}
+
+function isTenantListAPI(url) {
+    return /\/authority\/user\/pageTenantInfo$/.test(url)
+}
+
+fetchTrap(async (url, data) => {
+    await sleep(50)
+    if (!isTenantAccountPage()) return
+    if (isTenantListAPI(url)) {
+        await sleep(500)
+        const rows = Array.from(document.querySelectorAll('.ant-table-row'))
+        rows.forEach((row, index) => {
+            createButtonForRow(row, data.records[index])
         })
     }
-    
-    function fetchTrap() {
-        const oldFetch = window.fetch
-        const newFetch = async function(...args) {
-            const [url] = args
-            if (!requestAutoLogin || !url.endsWith('/authority/user/getLoginParam')) {
-                return oldFetch(...args)
-            }
-            const res = await oldFetch(...args)
-            const myRes = res.clone()
-            requestAutoLogin = false
-            const json = await myRes.json()
-            if (json.code === 10000) {
-                // TODO 获取token，获取系统号，写入localStorage，最后跳转页面
-            }
-            return res
-        }
-        Reflect.set(window, 'fetch', newFetch)
-    }
-    
-    function dom(str) {
-        const div = document.createElement('div')
-        div.innerHTML = str
-        return div.firstElementChild
-    }
-}
-
-// 客户管理端登录页面
-if (/\/user\/customLogin\/[\da-z]+/.test(location.pathname)) {
-    console.log('custom login page')
-}
+})
